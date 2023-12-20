46abc071fd24be50536c57bdae477c0197225c98
Sanchez70 <124312881+Sanchez70@users.noreply.github.com>
2023-12-19 20:05:34 -0500

    Proyecto con FireBase

---
+++ FirebaseM4A1/app/src/main/java/com/ista/firebasem4a1/MainActivity.java
@@ -0,0 +1,52 @@
+package com.ista.firebasem4a1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.dynamic.IFragmentWrapper;
+import com.google.firebase.Firebase;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
+    Button inicio;
+    TextView textView;
+    FirebaseUser user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        auth= FirebaseAuth.getInstance();
+        inicio= findViewById(R.id.logout);
+        textView= findViewById(R.id.userDetails);
+        user = auth.getCurrentUser();
+        if (user==null){
+            Intent intent= new Intent(getApplicationContext(), Login.class);
+            startActivity(intent);
+            finish();
+        }else{
+            textView.setText(user.getEmail());
+        }
+
+        inicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent= new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+    }
+}
